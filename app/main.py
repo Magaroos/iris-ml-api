e75132820b049529656521b69654510bd9b69544
Mohamed Magaroos <magaroos45@gmail.com>
2026-08-24 14:54:54 +0530

    Task 5 completed - ML model loaded into FastAPI

--- app/main.py
+++ app/main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
+import joblib
+import pandas as pd
 
 app = FastAPI()
+
+model = None  # global variable
+
+# ✅ Load model ONCE at startup
+@app.on_event("startup")
+def load_model():
+    global model
+    model = joblib.load("ml/saved_model/model.joblib")
+    print("✅ Model loaded successfully!")
 
 @app.get("/")
 def root():
     return {"message": "ML API is alive"}
 
+# ✅ Accept input and predict
 @app.post("/predict")
 def predict():
-    return {"prediction": "hardcoded_result"}
+    data = {
+        "sepal length (cm)": 5.1,
+        "sepal width (cm)": 3.5,
+        "petal length (cm)": 1.4,
+        "petal width (cm)": 0.2
+    }
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)
+
+    return {"prediction": int(prediction[0])}
